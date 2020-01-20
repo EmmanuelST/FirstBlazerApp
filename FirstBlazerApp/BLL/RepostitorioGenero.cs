47710b607c0242c862a0512951dff503c555bda8
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2020-01-19 22:19:33 -0400

    Actualizacion

---
+++ FirstBlazerApp/BLL/RepostitorioGenero.cs
@@ -0,0 +1,139 @@
+﻿using FirstBlazerApp.DAL;
+using FirstBlazerApp.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBlazerApp.BLL
+{
+    public class RepostitorioGenero
+    {
+
+        public static bool Guardar(Generos genero)
+        {
+            bool paso = false;
+
+            Contexto db = new Contexto();
+
+            try
+            {
+
+                db.Generos.Add(genero);
+                paso = db.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+
+
+
+            return paso;
+        }
+
+
+        public static bool Modificar(Generos genero)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(genero).State = EntityState.Modified;
+                paso = db.SaveChanges() > 0;
+                
+
+            }
+            catch (Exception) { throw; }
+            finally { db.Dispose(); }
+
+
+            return paso;
+        }
+
+
+        public static bool Eliminar(int Id)
+        {
+            bool paso = false;
+
+            Contexto db = new Contexto();
+            
+            try
+            {
+
+                Generos genero = db.Generos.Find(Id);
+
+                if(genero != null)
+                {
+                    db.Entry(genero).State = EntityState.Deleted;
+                    paso = db.SaveChanges() > 0;
+               
+                }
+
+
+            }
+            catch (Exception) { throw; }
+            finally { db.Dispose(); }
+
+
+
+            return paso;
+        }
+
+
+        public static Generos Buscar(int Id)
+        {
+            Generos genero = null;
+            Contexto db = new Contexto();
+
+            try
+            {
+                genero = db.Generos.Find(Id);
+
+
+
+            }catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+
+
+            return genero;
+        }
+
+
+
+        public static List<Generos> GetList()
+        {
+            List<Generos> lista = new List<Generos>();
+            Contexto db = new Contexto();
+
+            try
+            {
+                lista = db.Generos.Where(G => true).ToList();
+
+            }
+            catch (Exception) { throw; }
+            finally { db.Dispose(); }
+
+
+            return lista;
+            
+        }
+
+
+    }
+}
---
+++ FirstBlazerApp/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿
+using FirstBlazerApp.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBlazerApp.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Generos> Generos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database = RazorDb; Trusted_Connection = True; ");
+        }
+    }
+}
